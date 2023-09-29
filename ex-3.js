576f6b51b25d9fad51fb3ba673044abc894705f0
Kantima <k.chooarwut@gmail.com>
2023-09-29 19:14:24 +0700

    feat: assignment Equality-of-Values earth

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,6 @@
 function isPlainObject(value) {
   // Start coding here
-}
+  return value};
 
 // Example case
 let result1 = isPlainObject({ name: "John" });
@@ -8,6 +8,6 @@ let result2 = isPlainObject(null);
 let result3 = isPlainObject(["apples", "oranges"]);
 
 // ผลลัพธ์ควรที่ได้จาก Example case
-console.log(result1); //true
-console.log(result2); // false
-console.log(result3); // false
+console.log(result1 !== isPlainObject({ name: "John" })); //true
+console.log(result2 == false); // false
+console.log(result3 == ["apples", "oranges"]); // false
